019ae8348ab851e16823c0a7311cc3dad8705760
cedric <cedric@192.168.0.212>
2016-06-11 21:31:32 +0200

    added a plopkoek command added a plopkoek method

--- Calculator/src/me/trolking1/Calculator/Main.java
+++ Calculator/src/me/trolking1/Calculator/Main.java
@@ -1,8 +1,13 @@
 package me.trolking1.Calculator;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
 
 public class Main extends JavaPlugin {
 	
@@ -15,8 +20,26 @@ public class Main extends JavaPlugin {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player p= (Player) sender;
+		if (sender instanceof Player){
+			Player p = (Player) sender;
+			
+			if (label.equalsIgnoreCase("plopkoek")){
+				plopkoek(p);
+			}
+				
+				
+		}
+		
 		return false;
 	}
+	
+	public void plopkoek(Player p ){
+		ItemStack i = new ItemStack(Material.COOKIE);
+		ItemMeta im = i.getItemMeta();
+		im.setDisplayName("plopkoek");
+		i.setItemMeta(im);
+		p.getInventory().addItem(i);
 		
+	}
+	
 }
