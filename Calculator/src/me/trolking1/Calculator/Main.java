fc9f8565309b1eaa27df4d896df9eaa52b7a56f3
Gabriel D'Hondt <gabriel.dhondt@hotmail.com>
2016-06-11 21:03:23 +0200

    Added preparation for bukkit API lessons

--- Calculator/src/me/trolking1/Calculator/Main.java
+++ Calculator/src/me/trolking1/Calculator/Main.java
@@ -1,12 +1,20 @@
 package me.trolking1.Calculator;
 
-public class Main {
-	int total;
-	int total2;
-	
-	public static void main(String[] args) {
-		System.out.println("test");
-		System.out.println("test 2");
-	}
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		return false;
+	}
 }
